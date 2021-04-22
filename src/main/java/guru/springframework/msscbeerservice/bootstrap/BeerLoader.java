4ba2d80ea6f1d3e820c69cc89ab995a133777c67
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-04-22 18:09:18 +0800

    adding bootstrap - closes #7

---
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,47 @@
+package guru.springframework.msscbeerservice.bootstrap;
+
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import guru.springframework.msscbeerservice.web.domain.Beer;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0){
+
+            beerRepository.save(Beer.builder()
+                .beerName("Mango Bobs")
+                .beerStyle("IPA")
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(30001656565L)
+                .price(new BigDecimal(12.95))
+                .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(300016545665L)
+                    .price(new BigDecimal(11.95))
+                    .build());
+        }
+
+    }
+}
